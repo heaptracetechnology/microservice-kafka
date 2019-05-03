5e67d4a5cc7b37cec2900c2c37ebaf9603d29c28
Rohit Shetty <rohits@heaptrace.com>
2019-05-03 12:02:51 +0530

    updated test case

--- message/message_test.go
+++ message/message_test.go
@@ -11,34 +11,3 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
-
-var _ = Describe("Cron event subscribe", func() {
-	var argumentData Subscribe
-	argumentData.IsTesting = true
-	var data Data
-	data.Interval = 2
-	data.InitialDelay = 1
-	argumentData.Data = data
-	argumentData.Endpoint = "http://webhook.site/bfd1aea6-0562-4087-90a3-68efab7d0302"
-	requestBody := new(bytes.Buffer)
-	errr := json.NewEncoder(requestBody).Encode(argumentData)
-	if errr != nil {
-		log.Fatal(errr)
-	}
-
-	request, err := http.NewRequest("POST", "/subscribe", requestBody)
-	if err != nil {
-		log.Fatal(err)
-	}
-	recorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(TriggerCron)
-	handler.ServeHTTP(recorder, request)
-
-	Describe("Send Message", func() {
-		Context("SendMessage", func() {
-			It("Should result http.StatusOK", func() {
-				Expect(http.StatusOK).To(Equal(recorder.Code))
-			})
-		})
-	})
-})
