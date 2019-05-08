c780a19d8db9d7ef0efb8d6b1a60c520635c0c9f
ht68rohit <rohits@heaptrace.com>
2019-05-08 21:58:38 +0530

    Updated yml and code

--- kafka/kafka.go
+++ kafka/kafka.go
@@ -1,15 +1,16 @@
-package kafka 
- 
-import ( 
+package kafka
+
+import (
+	"context"
 	"encoding/json"
 	"fmt"
-	"net/http"
-	"github.com/confluentinc/confluent-kafka-go/kafka" 
 	"github.com/cloudevents/sdk-go"
-	"github.com/heaptracetechnology/microservice-kafka/result" 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/heaptracetechnology/microservice-kafka/result"
 	"log"
+	"net/http"
 	"net/url"
-	"context"
+	"os"
 	"time"
 )
 
@@ -22,7 +23,7 @@ type Subscribe struct {
 }
 
 type Data struct {
-	Topic     string `json:"topic"`
+	Topic string `json:"topic"`
 }
 
 type Message struct {
@@ -31,10 +32,9 @@ type Message struct {
 	StatusCode int    `json:"statuscode"`
 }
 
-
 type Produce struct {
-	Topic      string `json:"topic"`
-	Message    string `json:"message"`
+	Topic   string `json:"topic"`
+	Message string `json:"message"`
 }
 
 var Listener = make(map[string]Subscribe)
@@ -42,6 +42,8 @@ var rtmstarted bool
 var isConsumerRunning bool
 
 func Consume(responseWriter http.ResponseWriter, request *http.Request) {
+
+	var host = os.Getenv("HOST")
 
 	decoder := json.NewDecoder(request.Body)
 
@@ -51,14 +53,14 @@ func Consume(responseWriter http.ResponseWriter, request *http.Request) {
 		result.WriteErrorResponse(responseWriter, errr)
 		return
 	}
-	
-		c, _ := kafka.NewConsumer(&kafka.ConfigMap{
-			"bootstrap.servers": "192.168.1.89",
-			"group.id":          listener.Id,
-			"auto.offset.reset": "earliest",
-			"session.timeout.ms":    6000,
-		})
-	
+
+	c, _ := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":  host,
+		"group.id":           listener.Id,
+		"auto.offset.reset":  "earliest",
+		"session.timeout.ms": 6000,
+	})
+
 	Listener[listener.Id] = listener
 	if !rtmstarted {
 		go KafkaRTM(*c)
@@ -75,7 +77,7 @@ func KafkaRTM(c kafka.Consumer) {
 	for {
 		if len(Listener) > 0 {
 			for k, v := range Listener {
-				go getMessageUpdates(k, v,c)
+				go getMessageUpdates(k, v, c)
 				istest = v.IsTesting
 			}
 		} else {
@@ -90,8 +92,7 @@ func KafkaRTM(c kafka.Consumer) {
 	}
 }
 
-
-func getMessageUpdates(userid string, sub Subscribe,c kafka.Consumer) {
+func getMessageUpdates(userid string, sub Subscribe, c kafka.Consumer) {
 
 	contentType := "application/json"
 	t, err := cloudevents.NewHTTPTransport(
@@ -108,38 +109,36 @@ func getMessageUpdates(userid string, sub Subscribe,c kafka.Consumer) {
 		cloudevents.WithTimeNow(),
 	)
 
-
 	c.SubscribeTopics([]string{sub.Data.Topic, "^aRegex.*[Tt]opic"}, nil)
-		msg, err := c.ReadMessage(-1)
-		if err == nil {
-			source, err := url.Parse(sub.Endpoint)
-			event := cloudevents.Event{
-				Context: cloudevents.EventContextV01{
-					EventID:     sub.Id,
-					EventType:   "consume",
-					Source:      cloudevents.URLRef{URL: *source},
-					ContentType: &contentType,
-				}.AsV01(),
-				Data:msg.Value,
-			}
-			resp, err := cloudClient.Send(context.Background(), event)
-			if err != nil {
-				log.Printf("failed to send: %v", err)
-				fmt.Println(resp)
-			}
-
-
-			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-		} else {
-			// The client will automatically try to recover from all errors.
-			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+	msg, err := c.ReadMessage(-1)
+	if err == nil {
+		source, err := url.Parse(sub.Endpoint)
+		event := cloudevents.Event{
+			Context: cloudevents.EventContextV01{
+				EventID:     sub.Id,
+				EventType:   "consume",
+				Source:      cloudevents.URLRef{URL: *source},
+				ContentType: &contentType,
+			}.AsV01(),
+			Data: msg.Value,
 		}
+		resp, err := cloudClient.Send(context.Background(), event)
+		if err != nil {
+			log.Printf("failed to send: %v", err)
+			fmt.Println(resp)
+		}
+
+		fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+	} else {
+		// The client will automatically try to recover from all errors.
+		fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+	}
 }
-
-
 
 //Kafka service
 func ProduceStream(responseWriter http.ResponseWriter, request *http.Request) {
+
+	var host = os.Getenv("HOST")
 
 	decoder := json.NewDecoder(request.Body)
 
@@ -150,7 +149,7 @@ func ProduceStream(responseWriter http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "192.168.1.89"})
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": host})
 	if err != nil {
 		panic(err)
 	}
@@ -176,69 +175,15 @@ func ProduceStream(responseWriter http.ResponseWriter, request *http.Request) {
 		TopicPartition: kafka.TopicPartition{Topic: &produce.Topic, Partition: kafka.PartitionAny},
 		Value:          []byte(produce.Message),
 	}, nil)
-		if produceErr != nil {
-			log.Printf("failed to send: %v", err)
-			return
-		} else {
-			p.Flush(15 * 1000)
+	if produceErr != nil {
+		log.Printf("failed to send: %v", err)
+		return
+	} else {
+		p.Flush(15 * 1000)
 
-			message := Message{"true", "Topic created successfully.", http.StatusOK}
-			bytes, _ := json.Marshal(message)
-			result.WriteJsonResponse(responseWriter, bytes, http.StatusOK)
-		}
-		// Wait for message deliveries before shutting down
-		
-	
+		message := Message{"true", "Topic created successfully.", http.StatusOK}
+		bytes, _ := json.Marshal(message)
+		result.WriteJsonResponse(responseWriter, bytes, http.StatusOK)
+	}
+	// Wait for message deliveries before shutting down
 }
-
-
-
-
-
-// //Kafka service
-// func CreateTopic(responseWriter http.ResponseWriter, request *http.Request) {
-
-
-// 	decoder := json.NewDecoder(request.Body)
-
-// 	var produce Produce
-// 	errr := decoder.Decode(&produce)
-// 	if errr != nil {
-// 		result.WriteErrorResponse(responseWriter, errr)
-// 		return
-// 	}
-
-// 	p, err := kafka.NewClient(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	defer p.Close()
-
-// 	// Delivery report handler for produced messages
-// 	go func() {
-// 		for e := range p.Events() {
-// 			switch ev := e.(type) {
-// 			case *kafka.Message:
-// 				if ev.TopicPartition.Error != nil {
-// 					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-// 				} else {
-// 					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
-// 				}
-// 			}
-// 		}
-// 	}()
-
-// 	// Produce messages to topic (asynchronously)
-// 		produceErr := p.Produce(&kafka.Message{
-// 			TopicPartition: kafka.TopicPartition{Topic: &produce.Topic, Partition: kafka.PartitionAny},
-// 			Value:          []byte(produce.Message),
-// 		}, nil)
-// 		if produceErr != nil {
-// 			log.Printf("failed to send: %v", err)
-// 		}
-
-// 	// Wait for message deliveries before shutting down
-// 	p.Flush(15 * 1000)
-// }
-
